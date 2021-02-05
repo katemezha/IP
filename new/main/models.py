feb9cbf63b65488724803ccc37e32c7fd8f4d3ac
katemezha <44982634+katemezha@users.noreply.github.com>
2021-02-05 19:22:58 +0300

    m

--- new/main/models.py
+++ new/main/models.py
@@ -4,6 +4,14 @@ from django.db import models
 class Price(models.Model):
     title = models.CharField('Name', max_length=50)
     price = models.CharField('Price', max_length=50)
+    img = models.CharField('Img', max_length=50)
+
+class Sale(models.Model):
+    title = models.CharField('Name', max_length=50)
+    price = models.CharField('Price', max_length=50)
+    img = models.CharField('Img', max_length=50)
+
+
 
     def __str__(self):
         return self.title
